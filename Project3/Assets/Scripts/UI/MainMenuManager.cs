dcffac1af0340b39c5d45c5352560c0222fa9397
Aiden Pain <aiden.pain@gmail.com>
2025-10-06 21:50:53 -0400

    Pause Menu Bug Fix
    fixed the pause menu not working by adding code to show the cursor on pausing

--- Project3/Assets/Scripts/UI/MainMenuManager.cs
+++ Project3/Assets/Scripts/UI/MainMenuManager.cs
@@ -5,8 +5,15 @@ public class MainMenuManager : MonoBehaviour
 {
 	[SerializeField] string _GameplayScene;
 	
+	public void Start(){
+		Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+	}
+	
 	public void StartButtonClicked(){
 		Debug.Log("start!");
+		Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 		SceneManager.LoadScene(_GameplayScene);
 	}
 	
