10298ad2650bfd6e162cffe5de298905ce460e10
Donovan Peckham <do798543@ucf.edu>
2025-11-17 14:12:11 -0500

    Updated Shoal Enemies

--- Project3/Assets/Scripts/Enemy/EnemyVarieties/ShoalEnemy.cs
+++ Project3/Assets/Scripts/Enemy/EnemyVarieties/ShoalEnemy.cs
@@ -1,5 +1,46 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class ShoalEnemy : EnemyBase
 {
+    new void Update()
+    {
+        bool grounded = IsEnemyGrounded();
+        if (isPushed)
+        {
+            pushCooldown -= Time.deltaTime;
+        }
+        if (pushCooldown < 0)
+        {
+            if (!isGrabbed)
+            {
+                pushCooldown = 0;
+                isPushed = false;
+                agent.enabled = true;
+                rb.isKinematic = true;
+            }
+            enemyHealth.value -= 1;
+            if (enemyHealth.value <= 0)
+            {
+                enemyHealthScreen.SetActive(false);
+            }
+            Destroy(gameObject);
+        }
+        if (!grounded)
+        {
+            ResetSlapState();
+        }
+        if (grounded && wasGrounded && !isGrabbed && !isPushed)
+        {
+            // Debug.Log("Enemy just landed!");
+            rb.isKinematic = true;
+            agent.enabled = true;
+        }
+        wasGrounded = grounded;
+        if (agent.enabled && agent.isOnNavMesh)
+        {
+            ChasePlayer();
+        }
+    }
 }
