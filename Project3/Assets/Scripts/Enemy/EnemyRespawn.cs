082a2428cb00cc7fd9c21fa600667d11f3c441ec
Trackax <do798543@ucf.edu>
2025-11-18 13:33:08 -0500

    Updated Shoal Respawn when Out of Bounds

--- Project3/Assets/Scripts/Enemy/EnemyRespawn.cs
+++ Project3/Assets/Scripts/Enemy/EnemyRespawn.cs
@@ -4,6 +4,9 @@ public class EnemyRespawn : MonoBehaviour
 {
     public GameObject enemyRespawn;
     public GameObject enemyRespawn2;
+    public GameObject enemyRespawn3;
+    public GameObject enemyRespawn4;
+    public GameObject enemyRespawn5;
     public Transform respawnPoint;
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +21,36 @@ public class EnemyRespawn : MonoBehaviour
         if (enemyRespawn2 != null)
         {
             if (other.gameObject == enemyRespawn2 && this.gameObject.tag == "DontRespawn")
+            {
+                if (respawnPoint != null)
+                {
+                    transform.position = respawnPoint.position;
+                }
+            }
+        }
+        if (enemyRespawn3 != null)
+        {
+            if (other.gameObject == enemyRespawn3 && this.gameObject.tag == "DontRespawn")
+            {
+                if (respawnPoint != null)
+                {
+                    transform.position = respawnPoint.position;
+                }
+            }
+        }
+        if (enemyRespawn4 != null)
+        {
+            if (other.gameObject == enemyRespawn4 && this.gameObject.tag == "DontRespawn")
+            {
+                if (respawnPoint != null)
+                {
+                    transform.position = respawnPoint.position;
+                }
+            }
+        }
+        if (enemyRespawn4 != null)
+        {
+            if (other.gameObject == enemyRespawn4 && this.gameObject.tag == "DontRespawn")
             {
                 if (respawnPoint != null)
                 {
