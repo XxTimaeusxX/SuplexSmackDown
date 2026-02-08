773fb118e8a66c4efd5743a92d8836c17051e18a
ghostweever <ni103548@ucf.edu>
2026-02-08 17:16:09 -0500

    threw camera and camera change code
    the coee just needs to be looked at , and the triggers added. The code to transfer is there and its plug-in-play.
    its in as cinema final
    
    also i added all the voice lines for the boss

---
+++ Project3/Assets/Scripts/Camera/Cinema_final.cs
@@ -0,0 +1,109 @@
+using Unity.Cinemachine;
+using System.Collections;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.Audio;
+
+
+public class Cinema_final : MonoBehaviour
+{
+    [Header("Camera Settings")]
+    public GameObject introCameraObj;
+    public CinemachineCamera introCamera;
+    public CinemachineCamera freeLookCamera;
+    public CinemachineCamera towerCamera;
+
+    [Header("Trigger Settings")]
+    public GameObject player;
+    public float triggerRange = 25f;
+    private bool introPlayed = false;
+    private PlayerMovement playerMovementScript;
+
+    [Header("UI Settings")]
+    public GameObject introUI;
+
+    [Header("Sounds")]
+    public AudioClip introSound;
+
+    [Header("Camera Transition Settings")]
+    public float transitionSpeed = 2f;
+    private Transform introCameraTransform;
+    public float positionThreshold = 1f;
+    public float rotationThreshold = 2f;
+
+    public bool isMainMenuIntro;
+    private CinemachineCamera lastActiveCamera;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        playerMovementScript = player.GetComponent<PlayerMovement>();
+        introCameraTransform = introCamera.transform;
+        audioSource = GetComponent<AudioSource>();
+
+        if (!isMainMenuIntro)
+            introCameraObj.SetActive(false);
+
+        lastActiveCamera = freeLookCamera.Priority > towerCamera.Priority ? freeLookCamera : towerCamera;
+    }
+
+    private void Update()
+    {
+        if (!introPlayed && Vector3.Distance(transform.position, player.transform.position) <= triggerRange)
+        {
+            StartCoroutine(StartIntro());
+        }
+    }
+
+    private IEnumerator StartIntro()
+    {
+        introPlayed = true;
+        playerMovementScript.enabled = false;
+
+        lastActiveCamera = freeLookCamera.Priority > towerCamera.Priority ? freeLookCamera : towerCamera;
+
+        introCameraObj.SetActive(true);
+
+        introCamera.Priority = 100;
+
+        yield return new WaitForSeconds(0.5f);
+
+        while (!IsCameraInPosition(introCameraTransform))
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+
+        introUI.SetActive(true);
+        audioSource.Play();
+
+        yield return new WaitForSeconds(4f);
+        EndIntro();
+    }
+
+    private void EndIntro()
+    {
+        introUI.SetActive(false);
+        introCameraObj.SetActive(false);
+
+        introCamera.Priority = 1;
+
+        playerMovementScript.enabled = true;
+    }
+
+    private bool IsCameraInPosition(Transform targetTransform)
+    {
+        Transform mainCam = Camera.main.transform;
+
+        float positionDiff = Vector3.Distance(mainCam.position, targetTransform.position);
+        float rotationDiff = Quaternion.Angle(mainCam.rotation, targetTransform.rotation);
+
+        return positionDiff < positionThreshold && rotationDiff < rotationThreshold;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, triggerRange);
+    }
+}
