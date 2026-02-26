c4b7cf9cb24d44912363c5c108bdd8a63c410dc1
Donovan Peckham <do798543@ucf.edu>
2026-02-26 13:08:09 -0500

    Working on Boss2

--- Project3/Assets/Scripts/Bosses/GhostWrestler/Level2BossManager.cs
+++ Project3/Assets/Scripts/Bosses/GhostWrestler/Level2BossManager.cs
@@ -1,23 +1,101 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Level2BossManager : MonoBehaviour
 {
     [Header("Settings")]
     public float moveSpeed;
+    public float timeBetweenAttacks;
+    public float walkPointRange;
+    public float sightRange;
+    public float attackRange;
+    public Vector3 walkPoint;
 
     [Header("References")]
-    public GameObject player;
+    public Transform player;
     public GameObject intangibleBody;
     public GameObject solidBody;
+    public NavMeshAgent agent;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    [Header("Bools")]
+    public bool alreadyAttacked;
+    public bool walkPointSet;
+    public bool playerInSightRange;
+    public bool playerInAttackRange;
 
     [Header("Strings")]
     public string flower;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
-        if (collision.gameObject.CompareTag(flower))
+        agent.speed = moveSpeed;
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInSightRange && !playerInAttackRange)
         {
-            Debug.Log("Collide");
+            Patroling();
         }
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        Vector3 targetPostition = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.LookAt(targetPostition);
+        if (!alreadyAttacked)
+        {
+            //Attack
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 }
