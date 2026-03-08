2fe9b8eeb7bebe324e987991366785c978f026a5
Ovidio Juan <149029398+XxTimaeusxX@users.noreply.github.com>
2026-03-08 04:31:49 -0400

    fixed ghost dancer tagging for shockwave and grabbing

--- Project3/Assets/Scripts/Player/SuplexHitboxCaller.cs
+++ Project3/Assets/Scripts/Player/SuplexHitboxCaller.cs
@@ -21,7 +21,7 @@ public class SuplexHitboxCaller : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Only react if the collider is tagged as "Enemy" and we have a PlayerSuplex reference
-        if (other.CompareTag("Enemy") || other.CompareTag("DontRespawn") || other.CompareTag("Macro") || other.CompareTag("Drone") || other.CompareTag("Solid") && playerSuplex != null)
+        if (other.CompareTag("Enemy") || other.CompareTag("DontRespawn") || other.CompareTag("Macro") || other.CompareTag("Drone") || other.CompareTag("Solid") || other.CompareTag("GhostDancer") && playerSuplex != null)
         {
            //  Debug.Log("hitboxcollider called");
             if (other.CompareTag("Drone"))
--- Project3/Assets/Scripts/Player/abilities/Shockwave.cs
+++ Project3/Assets/Scripts/Player/abilities/Shockwave.cs
@@ -37,7 +37,7 @@ public class Shockwave : MonoBehaviour
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
-        if (hit.gameObject.CompareTag("Enemy"))
+        if (hit.gameObject.CompareTag("Enemy")|| hit.gameObject.CompareTag("GhostDancer"))
         {
             Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
             body.AddForce(pushDir * pushForce, ForceMode.Impulse);
