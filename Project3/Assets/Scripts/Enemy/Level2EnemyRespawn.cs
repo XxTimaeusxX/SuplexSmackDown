11190f567f39cabf7437661a7be32c29196d3189
Donovan Peckham <do798543@ucf.edu>
2026-04-05 20:21:04 -0400

    Update Level2EnemyRespawn.cs

--- Project3/Assets/Scripts/Enemy/Level2EnemyRespawn.cs
+++ Project3/Assets/Scripts/Enemy/Level2EnemyRespawn.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Level2EnemyRespawn : MonoBehaviour
 {
-    public Transform repsawnPoint;
+    private Vector3 respawnPoint;
+
+    private void Start()
+    {
+        respawnPoint = transform.position;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Respawn"))
         {
-            transform.position = collision.transform.position;
+            transform.position = respawnPoint;
         }
     }
 }
