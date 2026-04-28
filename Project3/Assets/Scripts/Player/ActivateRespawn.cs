ca06590e79eac575bab98abc87d8678a00b3eafc
Donovan Peckham <do798543@ucf.edu>
2026-04-28 11:55:28 -0400

    Updated Level 1 Player Respawn

--- Project3/Assets/Scripts/Player/ActivateRespawn.cs
+++ Project3/Assets/Scripts/Player/ActivateRespawn.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class ActivateRespawn : MonoBehaviour
 {
+    [Header("Activate Respawns")]
+    public GameObject actiavteRespawn1;
+    public GameObject actiavteRespawn2;
+    public GameObject actiavteRespawn3;
+
+    [Header("RespawnTriggers")]
+    public GameObject respawnTrigger1;
+    public GameObject respawnTrigger2;
+    public GameObject respawnTrigger3;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == actiavteRespawn1)
+        {
+            respawnTrigger1.SetActive(true);
+        }
+        if(other.gameObject == actiavteRespawn2)
+        {
+            respawnTrigger2.SetActive(true);
+        }
+        if (other.gameObject == actiavteRespawn3)
+        {
+            respawnTrigger3.SetActive(true);
+        }
     }
 }
