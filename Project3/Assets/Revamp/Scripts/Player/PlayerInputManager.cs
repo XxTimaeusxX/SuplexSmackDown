841fc83c347c61ff693e9ada2df31bb47321d393
Donovan Peckham <do798543@ucf.edu>
2026-05-12 00:52:01 -0400

    Reworking Player Movement/Rotation

---
+++ Project3/Assets/Revamp/Scripts/Player/PlayerInputManager.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public static PlayerInputManager instance;
+
+    PlayerControls playerControls;
+
+    [SerializeField] Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
+    public float moveAmount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.activeSceneChanged += OnSceneChange;
+    }
+
+    private void OnSceneChange(Scene oldScene, Scene newScene)
+    {
+        if (newScene.buildIndex == WorldSaveGameManager.instance.GetWorldSceneIndex())
+        {
+            instance.enabled = true;
+        }
+        else
+        {
+            instance.enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= OnSceneChange;
+    }
+
+    private void Update()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput));
+
+        if (moveAmount <= 0.5 && moveAmount > 0)
+        {
+            moveAmount = 0.5f;
+        }
+        else if (moveAmount > 0.5 && moveAmount <= 1)
+        {
+            moveAmount = 1;
+        }
+    }
+}
---
+++ Project3/Assets/Revamp/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public CharacterController characterController;
+    PlayerMovementManager playerMovementManager;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        playerMovementManager = GetComponent<PlayerMovementManager>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        playerMovementManager.HandleAllMovement();
+    }
+}
